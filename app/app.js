aaa4985704ade833f5673b1fa3ecb626a6e6f76e
olti6666 <itbpotiran@gmail.com>
2026-08-01 22:28:07 +0200

    Restore application source code

---
+++ app/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+
+    res.send("Enterprise CI/CD Application Running");
+
+});
+
+
+app.get("/health", (req, res) => {
+
+    res.status(200).send("OK");
+
+});
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
+
+});
