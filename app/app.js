2d07b1cc51735abb2abf6333b64acee6f07758ae
olti6666 <itbpotiran@gmail.com>
2026-08-01 23:17:50 +0200

    Resolve app.js merge conflict

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-<<<<<<< HEAD
 const PORT = process.env.PORT || 3000;
 
 const ENV = process.env.ENVIRONMENT || "LOCAL";
+
 
 app.get("/", (req, res) => {
 
@@ -13,28 +13,14 @@ app.get("/", (req, res) => {
 
 });
 
-=======
-const PORT = 3000;
 
-
-app.get("/", (req, res) => {
-
-    res.send("Enterprise CI/CD Application Running");
-
-});
-
-
->>>>>>> main
 app.get("/health", (req, res) => {
 
     res.status(200).send("OK");
 
 });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> main
 app.listen(PORT, () => {
 
     console.log(`Server running on port ${PORT}`);
